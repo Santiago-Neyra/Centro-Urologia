89c8784e14f8a9c98c6b2e617794d9c813f5effd
Santiago-Neyra <santineyra97@gmail.com>
2022-05-25 00:06:12 -0300

    contacto

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,19 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../Footer/Footer.css"
 import { BsInstagram } from 'react-icons/bs';
+import {BsFacebook} from "react-icons/bs"
+import {BsWhatsapp} from "react-icons/bs"
 export const Footer=()=>{
     return(
         <div className="contenedor-footer">
             <div className="row row-1-footer">
-                <div className="col-lg-3 justify-content-center align-items-center"><img src="https://i.postimg.cc/GhRrq0sD/a0831456-7d61-4379-b9b5-d6bb43160054-1.png"></img></div>
-                <div className="col-lg-6 d-flex align-items-center justify-content-center cont-dos-footer">
+                <div className="col-lg-3 justify-content-center align-items-center"><img className="icon-footer" src="https://i.postimg.cc/GhRrq0sD/a0831456-7d61-4379-b9b5-d6bb43160054-1.png"></img></div>
+                <div className="col-lg-6 d-none d-sm-flex align-items-center justify-content-center cont-dos-footer">
                     <Link to="/">Inicio</Link>
                     <Link to="/Nosotros">Nosotros</Link>
                     <Link to="/Practicas">Prácticas</Link>
                     <Link to="/Tecnologias">Tecnologías</Link>
                     <Link to="/Contacto">Contacto</Link>
                 </div>    
-                <div className="col-lg-3 d-flex justify-content-center align-items-center"><a href="#"><  BsInstagram style={{color:"black"}} > </BsInstagram></a></div>
+                <div className="col-lg-3 d-flex justify-content-center align-items-center">
+                    <a href="#">
+                    <BsInstagram className="icons" style={{color:"black"}} > </BsInstagram></a>
+                    <a href="https://www.facebook.com/Centro-de-Urología-CDU-478609279004909" target={"_blank"}>
+                    <BsFacebook className="icons"  style={{color:"black"}} > </BsFacebook></a>
+                    <a href="#">
+                    <BsWhatsapp className="icons" style={{color:"black"}} > </BsWhatsapp></a>
+                    </div>
             </div>
             <div className="row-2-footer">
                 <a>© 2022 All rights reserved</a>
